8f3749372a1b8bfe23b1242ede30864c254ae0df
Pavan Rikhi <pavan.rikhi@gmail.com>
2017-08-26 00:59:22 -0400

    [#1104] Add Categories to Product Details API Route
    Nest a Product's Categories under a `categories` key.
    
    Refs #1104

--- server/src/Routes.hs
+++ server/src/Routes.hs
@@ -7,7 +7,7 @@ module Routes
     ) where
 
 import Data.Aeson ((.=), ToJSON(..), object)
-import Database.Persist ((==.), Entity(..), selectList, getBy)
+import Database.Persist ((==.), (<-.), Entity(..), selectList, getBy)
 import Servant ((:>), Capture, Get, JSON, throwError, err404)
 
 import Models
@@ -21,26 +21,30 @@ data ProductDetailsData =
         { pddProduct :: Entity Product
         , pddVariants :: [Entity ProductVariant]
         , pddSeedAttribute :: Maybe (Entity SeedAttribute)
+        , pddCategories :: [Entity Category]
         } deriving (Show)
 
 instance ToJSON ProductDetailsData where
-    toJSON ProductDetailsData { pddProduct = prod, pddVariants = variants, pddSeedAttribute = maybeAttr } =
-        object [ "product" .= toJSON prod
-               , "variants" .= map toJSON variants
-               , "seedAttribute" .= toJSON maybeAttr
+    toJSON productData =
+        object [ "product" .= toJSON (pddProduct productData)
+               , "variants" .= map toJSON (pddVariants productData)
+               , "seedAttribute" .= toJSON (pddSeedAttribute productData)
+               , "categories" .= map toJSON (pddCategories productData)
                ]
 
 type ProductDetailsRoute =
     Capture "slug" T.Text :> Get '[JSON] ProductDetailsData
 
+-- TODO: Use Esqueleto library to make 1 query instead of 4
 productDetailsRoute :: T.Text -> App ProductDetailsData
 productDetailsRoute slug = do
         maybeProduct <- runDB . getBy $ UniqueProductSlug slug
         case maybeProduct of
             Nothing ->
                 throwError err404
-            Just prod@(Entity productId _) -> do
-                (variants, maybeAttribute) <- runDB $
-                    (,) <$> selectList [ProductVariantProductId ==. productId] []
+            Just e@(Entity productId prod) -> do
+                (variants, maybeAttribute, categories) <- runDB $
+                    (,,) <$> selectList [ProductVariantProductId ==. productId] []
                         <*> getBy (UniqueAttribute productId)
-                return $ ProductDetailsData prod variants maybeAttribute
+                        <*> selectList [CategoryId <-. productCategoryIds prod] []
+                return $ ProductDetailsData e variants maybeAttribute categories
