adcfb96eccf84bb0519b9cbe4957e4dc4cddbf57
Pavan Rikhi <pavan.rikhi@gmail.com>
2017-08-27 18:29:52 -0400

    Fix Path Rewriting of Client Dev Server

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -112,7 +112,7 @@ module.exports = {
       '/api/*': {
         target: 'http://localhost:3000',
         changeOrigin: true,
-        pathRewrite: { "^/api/": "" },
+        pathRewrite: { "^/api/": "/" },
       },
       '/media/*': {
         target: 'http://localhost:3000',
