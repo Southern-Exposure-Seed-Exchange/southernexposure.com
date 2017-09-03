8640ae4a330859680ef2ea0b26cb0bf5c7474b1a
Pavan Rikhi <pavan.rikhi@gmail.com>
2017-09-03 05:55:52 -0400

    [#1108] Add Contact Information to Footer
    Refs #1108

--- client/src/SiteUI/Footer.elm
+++ client/src/SiteUI/Footer.elm
@@ -1,20 +1,45 @@
 module SiteUI.Footer exposing (view)
 
-import Html exposing (Html, h4, div, text, node)
-import Html.Attributes exposing (id, class)
+import Html exposing (..)
+import Html.Attributes exposing (id, class, href, target, title)
 
 
 view : Html msg
 view =
-    div [ id "footer", class "container" ]
-        [ node "footer"
-            []
-            [ div [ class "row" ]
-                [ div [ class "col-sm-4" ] [ h4 [ class "mt-3" ] [ text "Information" ] ]
-                , div [ class "col-sm-4" ] [ h4 [ class "mt-3" ] [ text "Important Links" ] ]
-                , div [ class "col-sm-4" ] [ h4 [ class "mt-3" ] [ text "Contact Us" ] ]
-                , div [ class "col-sm-12 text-center" ]
-                    [ text "Copyright © 2017 Southern Exposure Seed Exchange" ]
+    let
+        break =
+            br [] []
+
+        contactAddress =
+            address []
+                [ strong [] [ text "Southern Exposure Seed Exchange" ]
+                , break
+                , text "P.O. Box 460"
+                , break
+                , text "Mineral, Virginia 23117"
+                , break
+                , a [ href "mailto:gardens@southernexposure.com?subject=SESE Website Contact", target "_blank" ]
+                    [ text "gardens@southernexposure.com" ]
+                , break
+                , abbr [ title "Phone" ] [ text "P:" ]
+                , text " (540) 894-9480"
+                , break
+                , abbr [ title "Fax" ] [ text "F:" ]
+                , text " (540) 894-9481"
+                ]
+    in
+        div [ id "footer", class "container" ]
+            [ node "footer"
+                []
+                [ div [ class "row" ]
+                    [ div [ class "col-sm-4" ] [ h4 [ class "mt-3" ] [ text "Information" ] ]
+                    , div [ class "col-sm-4" ] [ h4 [ class "mt-3" ] [ text "Important Links" ] ]
+                    , div [ class "col-sm-4" ]
+                        [ h4 [ class "mt-3" ] [ text "Contact Us" ]
+                        , contactAddress
+                        ]
+                    , div [ class "col-sm-12 text-center" ]
+                        [ text "Copyright © 2017 Southern Exposure Seed Exchange" ]
+                    ]
                 ]
             ]
-        ]
