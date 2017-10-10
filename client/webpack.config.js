15dc3510476ede075b99193e89c3114dd63b7730
Pavan Rikhi <pavan.rikhi@gmail.com>
2017-10-10 03:01:15 -0400

    Use Babel to Transpile Webpack Output
    This fixes production builds since Uglify doesn't support syntax like
    `var [a,b] = someList;`.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -42,7 +42,22 @@ module.exports = {
           },
         ],
       },
-
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/,],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ["env", {
+                "targets": {
+                  "browsers": [">= 1%",]
+                },
+              }],
+            ],
+          },
+        },
+      },
       {
         test: /\.sass$/,
         exclude: [/node_modules/,],
