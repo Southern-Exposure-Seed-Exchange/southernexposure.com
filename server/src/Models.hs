0b553a7c918848eb1a5bd09696196ba289ebe744
Pavan Rikhi <pavan.rikhi@gmail.com>
2017-10-20 10:28:19 -0400

    Remove Unused Models.Classes Module

--- server/src/Models.hs
+++ server/src/Models.hs
@@ -1,10 +1,8 @@
 module Models
-    ( module Models.Classes
-    , module Models.DB
+    ( module Models.DB
     , module Models.Utils
     )
     where
 
-import Models.Classes
 import Models.DB
 import Models.Utils
