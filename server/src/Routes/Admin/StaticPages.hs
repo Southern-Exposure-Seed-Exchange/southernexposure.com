efc9ccec496f4af13164d381a77c1bacf82cd127
Pavan Rikhi <pavan.rikhi@gmail.com>
2019-11-04 14:46:50 -0500

    [#1438] Add API Route for Creating New Pages
    Add an admin API route to validate & create new Page rows.
    
    Refs #1438

--- server/src/Routes/Admin/StaticPages.hs
+++ server/src/Routes/Admin/StaticPages.hs
@@ -7,26 +7,35 @@ module Routes.Admin.StaticPages
     , staticPageRoutes
     ) where
 
-import Data.Aeson (ToJSON(..), (.=), object)
-import Database.Persist (Entity(..), SelectOpt(Asc), selectList)
-import Servant ((:>), AuthProtect, Get, JSON)
+import Data.Aeson (ToJSON(..), FromJSON(..), (.=), (.:), withObject, object)
+import Database.Persist (Entity(..), SelectOpt(Asc), selectList, insert)
+import Servant ((:<|>)(..), (:>), AuthProtect, ReqBody, Get, Post, JSON)
+import Text.HTML.SanitizeXSS (sanitize)
 
-import Auth (WrappedAuthToken, Cookied, withAdminCookie)
-import Models (Page(..), PageId, EntityField(PageName))
+import Auth (WrappedAuthToken, Cookied, withAdminCookie, validateAdminAndParameters)
+import Models (Page(..), PageId, EntityField(PageName), Unique(UniquePageSlug), slugify)
 import Server (App, runDB)
+import Validation (Validation(..))
 
 import qualified Data.Text as T
+import qualified Validation as V
 
 
 type StaticPageAPI =
          "list" :> PageListRoute
+    :<|> "new" :> NewPageRoute
 
 type StaticPageRoutes =
          (WrappedAuthToken -> App (Cookied PageListData))
+    :<|> (WrappedAuthToken -> NewPageParameters -> App (Cookied PageId))
 
 staticPageRoutes :: StaticPageRoutes
 staticPageRoutes =
          pageListRoute
+    :<|> newPageRoute
+
+
+-- LIST
 
 
 type PageListRoute =
@@ -73,3 +82,53 @@ pageListRoute = flip withAdminCookie $ const $
             , lpSlug = pageSlug page
             , lpContent = pageContent page
             }
+
+
+-- NEW
+
+
+type NewPageRoute =
+       AuthProtect "cookie-auth"
+    :> ReqBody '[JSON] NewPageParameters
+    :> Post '[JSON] (Cookied PageId)
+
+data NewPageParameters =
+    NewPageParameters
+        { nppTitle :: T.Text
+        , nppSlug :: T.Text
+        , nppContent :: T.Text
+        } deriving (Show)
+
+instance FromJSON NewPageParameters where
+    parseJSON = withObject "NewPageParameters" $ \v -> do
+        nppTitle <- v .: "title"
+        nppSlug <- v .: "slug"
+        nppContent <- v .: "content"
+        return NewPageParameters {..}
+
+instance Validation NewPageParameters where
+    validators NewPageParameters {..} = do
+        slugDoesntExist <- V.doesntExist $ UniquePageSlug nppSlug
+        return
+            [ ( "title"
+              , [ V.required nppTitle ]
+              )
+            , ( "slug"
+              , [ V.required nppSlug
+                , ( "A Page with this slug already exists.", slugDoesntExist )
+                ]
+              )
+            , ( "content"
+              , [ V.required nppContent ]
+              )
+            ]
+
+newPageRoute :: WrappedAuthToken -> NewPageParameters -> App (Cookied PageId)
+newPageRoute = validateAdminAndParameters $ \_ NewPageParameters {..} -> do
+    let newPage =
+            Page
+                { pageName = sanitize nppTitle
+                , pageSlug = slugify nppSlug
+                , pageContent = sanitize nppContent
+                }
+    runDB $ insert newPage
