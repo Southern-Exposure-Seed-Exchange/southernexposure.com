6c75b9f691c32cc9db8a560dd2f7d7700f061b56
Pavan Rikhi <pavan.rikhi@gmail.com>
2019-11-23 07:49:20 -0500

    [#1144] Add Route for Google Merchant Product Feed
    Add a `/merchant-feed.xml` route for generating the product data feed to
    integrate the website with Google Merchant.
    
    Add a MerchantFeed module containing the types & an XML rendering
    function based on the Merchant Feed's product data specification.
    
    Closes #1144

---
+++ server/src/MerchantFeed.hs
@@ -0,0 +1,197 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+module MerchantFeed
+    ( renderFeed
+    , Product(..)
+    , PriceData(..)
+    , Availability(..)
+    , PricingMeasure(..)
+    , ProductIdentifiers(..)
+    , ProductDescription(..)
+    , Condition(..)
+    ) where
+
+{-| This module provides types & render functions for generating a Google
+Merchant Product Feed in RSS2.0 format:
+
+https://support.google.com/merchants/answer/7052112
+
+-}
+
+import Data.Monoid ((<>))
+import Data.Scientific (Scientific, FPFormat(..), formatScientific)
+import Data.Time (UTCTime, formatTime, defaultTimeLocale)
+import GHC.Generics (Generic)
+import Text.XML.Generator
+    ( (<#>), Xml, Elem, XmlOutput, Namespace, xrender, doc, defaultDocInfo, xelemQ
+    , namespace, xelems, xelemWithText, xtext, xelem, xattr, xempty
+    )
+
+import qualified Data.Text as T
+
+
+renderFeed :: XmlOutput x => [Product] -> x
+renderFeed ps =
+    xrender $ doc defaultDocInfo $
+        xelem "rss" (xattr "version" "2.0" <#> channel)
+  where
+    channel =
+        xelem "channel" $ xelems $
+            [ xelemWithText "title" "Southern Exposure Seed Exchange"
+            , xelemWithText "link" "https://www.southernexposure.com"
+            , xelemWithText "description" "Southern Exposure Seed Exchange's Google Products Feed"
+            ]
+            ++ map renderProduct ps
+
+
+gNS :: Namespace
+gNS = namespace "g" "http://base.google.com/ns/1.0"
+
+gElemWithText :: T.Text -> T.Text -> Xml Elem
+gElemWithText name content =
+    xelemQ gNS name $ xtext content
+
+
+data Product =
+    Product
+        { pId :: T.Text
+        , pTitle :: T.Text
+        , pDescription :: T.Text
+        , pLink :: T.Text
+        , pImageLink :: T.Text
+        , pProductType :: [T.Text]
+        -- ^ Internal Category Hierarchy
+        , pGoogleProductType :: Maybe T.Text
+        -- ^ Google Category Code or Hierarchy
+        , pPriceData :: PriceData
+        , pIdentifiers :: ProductIdentifiers
+        , pProductDescription :: ProductDescription
+        } deriving (Show, Read, Eq, Generic)
+
+renderProduct :: Product -> Xml Elem
+renderProduct Product {..} =
+    xelem "item" $ xelems $
+        [ gElemWithText "id" pId
+        , gElemWithText "title" pTitle
+        , gElemWithText "description" pDescription
+        , gElemWithText "link" pLink
+        , gElemWithText "image_link" pImageLink
+        , gElemWithText "product_type" $ T.intercalate " > " pProductType
+        , maybe xempty (gElemWithText "google_product_category") pGoogleProductType
+        ]
+        ++ renderPriceData pPriceData
+        ++ renderProductIdentifiers pIdentifiers
+        ++ renderProductDescription pProductDescription
+
+
+data PriceData =
+    PriceData
+        { pdAvailability :: Availability
+        , pdPrice :: (Scientific, T.Text)
+        -- ^ Price & Currency Code
+        , pdSalePrice :: Maybe (Scientific, T.Text)
+        -- ^ Sale Price & Currency Code
+        , pdSaleDate :: Maybe (UTCTime, UTCTime)
+        -- ^ Sale Start & End Dates
+        , pdPricingMeasure :: Maybe PricingMeasure
+        } deriving (Show, Read, Eq, Generic)
+
+renderPriceData :: PriceData -> [Xml Elem]
+renderPriceData PriceData {..} =
+    [ gElemWithText "availability" $ renderAvailability pdAvailability
+    , gElemWithText "price" $ renderPrice pdPrice
+    , maybe xempty (gElemWithText "sale_price" . renderPrice) pdSalePrice
+    , flip (maybe xempty) pdSaleDate $ \(start, end) ->
+        gElemWithText "sale_price_effective_date"
+            $ renderTime start <> " / " <> renderTime end
+    , maybe xempty (gElemWithText "unit_pricing_measure" . renderPricingMeasure) pdPricingMeasure
+    ]
+
+renderPrice :: (Scientific, T.Text) -> T.Text
+renderPrice (price, currency) =
+    T.pack (formatScientific Fixed Nothing price) <> " " <> currency
+
+renderTime :: UTCTime -> T.Text
+renderTime =
+    T.pack . formatTime defaultTimeLocale "%FT%T%z"
+
+
+data Availability
+    = InStock
+    | OutOfStock
+    | PreOrder
+    deriving (Show, Read, Eq, Enum, Bounded, Generic)
+
+renderAvailability :: Availability -> T.Text
+renderAvailability = \case
+    InStock ->
+        "in stock"
+    OutOfStock ->
+        "out of stock"
+    PreOrder ->
+        "preorder"
+
+data PricingMeasure
+    = Grams Scientific
+    | Count Integer
+    deriving (Show, Read, Eq, Generic)
+
+renderPricingMeasure :: PricingMeasure -> T.Text
+renderPricingMeasure = \case
+    Grams g ->
+        T.pack (formatScientific Fixed Nothing g) <> "g"
+    Count i ->
+        T.pack (show i) <> "ct"
+
+
+data ProductIdentifiers =
+    ProductIdentifiers
+        { piBrand :: T.Text
+        , piMPN :: T.Text
+        } deriving (Show, Read, Eq, Generic)
+
+renderProductIdentifiers :: ProductIdentifiers -> [Xml Elem]
+renderProductIdentifiers ProductIdentifiers {..} =
+    [ gElemWithText "brand" piBrand
+    , gElemWithText "MPN" piMPN
+    ]
+
+data ProductDescription =
+    ProductDescription
+        { pdCondition :: Condition
+        , pdAdult :: Bool
+        , pdIsBundle :: Bool
+        , pdItemGroupId :: Maybe T.Text
+        } deriving (Show, Read, Eq, Generic)
+
+renderProductDescription :: ProductDescription -> [Xml Elem]
+renderProductDescription ProductDescription {..} =
+    [ gElemWithText "condition" $ renderCondition pdCondition
+    , gElemWithText "adult" $ renderBool pdAdult
+    , gElemWithText "is_bundle" $ renderBool pdIsBundle
+    , maybe xempty (gElemWithText "item_group_id") pdItemGroupId
+    ]
+
+data Condition
+    = New
+    | Refurbished
+    | Used
+    deriving (Show, Read, Eq, Bounded, Enum, Generic)
+
+renderCondition :: Condition -> T.Text
+renderCondition = \case
+    New ->
+        "new"
+    Refurbished ->
+        "refurbished"
+    Used ->
+        "used"
+
+renderBool :: Bool -> T.Text
+renderBool b =
+    if b then
+        "yes"
+    else
+        "no"
--- server/src/Routes/Feeds.hs
+++ server/src/Routes/Feeds.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE TypeOperators #-}
@@ -7,35 +8,49 @@ module Routes.Feeds
     , feedRoutes
     ) where
 
+import Control.Concurrent.STM (readTVarIO)
+import Control.Monad.Reader (asks, liftIO)
+import Data.Maybe (listToMaybe)
 import Data.Monoid ((<>))
-import Database.Persist ((==.), Entity(..), selectList)
+import Data.Scientific (Scientific, scientific)
+import Data.Time (UTCTime, getCurrentTime)
+import Database.Persist ((==.), (>=.), (<=.), Entity(..), selectList)
 import Servant ((:<|>)(..), (:>), Get)
 
+import Cache (Caches(..), CategoryPredecessorCache, queryCategoryPredecessorCache)
+import Config (Config(getCaches))
 import Sitemap
     ( Sitemap(..), SitemapUrl(..), ChangeFrequency(..), renderSitemap
     , SitemapIndex(..), IndexEntry(..), renderSitemapIndex
     )
 import Models
+import Models.Fields (Milligrams(..), LotSize(..), renderLotSize, toDollars)
 import Routes.Utils (XML)
 import Server (App, runDB)
 
 import qualified Data.ByteString.Lazy as LBS
+import qualified Data.Map.Strict as M
 import qualified Data.Text as T
+import qualified Database.Esqueleto as E
+import qualified MerchantFeed as GMerch
 
 
 type FeedAPI =
          "sitemap.xml" :> SitemapRoute
     :<|> "sitemap-index.xml" :> SitemapIndexRoute
+    :<|> "merchant-feed.xml" :> MerchantFeedRoute
 
 
 type FeedRoutes =
          App LBS.ByteString
+    :<|> App LBS.ByteString
     :<|> App LBS.ByteString
 
 feedRoutes :: FeedRoutes
 feedRoutes =
          sitemapRoute
     :<|> sitemapIndexRoute
+    :<|> merchantFeedRoute
 
 
 -- SITEMAPS
@@ -116,3 +131,179 @@ sitemapIndexRoute =
         [ IndexEntry { indexLocation = "/sitemap.xml", indexLastModified = Nothing }
         , IndexEntry { indexLocation = "/blog/sitemap.xml.gz", indexLastModified = Nothing }
         ]
+
+
+-- GOOGLE MERCHANT FEED
+
+
+type MerchantFeedRoute =
+       Get '[XML] LBS.ByteString
+
+merchantFeedRoute :: App LBS.ByteString
+merchantFeedRoute = do
+    currentTime <- liftIO getCurrentTime
+    (variantSales, categorySales, products, categories) <- runDB $ do
+        vs <- map entityVal <$> selectList
+            [ ProductSaleStartDate <=. currentTime
+            , ProductSaleEndDate >=. currentTime
+            ]
+            []
+        catSale <- map entityVal <$> selectList
+            [ CategorySaleStartDate <=. currentTime
+            , CategorySaleEndDate >=. currentTime
+            ]
+            []
+        ps <- E.select $ E.from $ \(p `E.InnerJoin` v) -> do
+            E.on $ v E.^. ProductVariantProductId E.==. p E.^. ProductId
+            E.where_ $ p E.^. ProductIsActive E.||. v E.^. ProductVariantIsActive
+            return (p, v)
+        cs <- selectList [] []
+        return (vs, catSale, ps, cs)
+    let categoryMap = foldr (\(Entity cId c) m -> M.insert cId c m) M.empty categories
+    categoryCache <- asks getCaches >>= fmap getCategoryPredecessorCache . liftIO . readTVarIO
+    let convert = convertProduct categoryCache categoryMap variantSales categorySales
+    return $ GMerch.renderFeed $ map convert products
+
+convertProduct
+    :: CategoryPredecessorCache
+    -> M.Map CategoryId Category
+    -> [ProductSale]
+    -> [CategorySale]
+    -> (Entity Product, Entity ProductVariant)
+    -> GMerch.Product
+convertProduct predCache categoryMap prodSales catSales (Entity _ prod, Entity variantId variant) =
+    let
+        fullSku =
+            productBaseSku prod <> productVariantSkuSuffix variant
+        lotSize =
+            maybe "" (\ls -> ", " <> renderLotSize ls)
+                $ productVariantLotSize variant
+        baseUrl =
+            "https://www.southernexposure.com"
+        productUrl =
+            baseUrl <> "/products/"
+        imageUrl =
+            baseUrl <> "/media/products/originals/"
+        categoryHierarchy =
+            maybe [] makeHierarchy $ listToMaybe (productCategoryIds prod)
+        measure =
+            productVariantLotSize variant >>= lotSizeToMeasurement
+        googleCategory
+            | hierarchyContains "Supplies" =
+                "3173"
+            | hierarchyContains "Books" =
+                "783"
+            | otherwise =
+                "2802"
+        hierarchyContains search =
+            any (search `T.isInfixOf`) categoryHierarchy
+    in
+    GMerch.Product
+        { pId = fullSku
+        , pTitle = productName prod <> lotSize
+        , pDescription = productLongDescription prod
+        , pLink = productUrl <> productSlug prod <> "/"
+        , pImageLink = imageUrl <> productImageUrl prod
+        , pProductType = categoryHierarchy
+        , pGoogleProductType = Just googleCategory
+        , pPriceData =
+            GMerch.PriceData
+                { pdAvailability =
+                    if productVariantQuantity variant > 0 then
+                        GMerch.InStock
+                    else
+                        GMerch.OutOfStock
+                , pdPrice =
+                    (toDollars $ productVariantPrice variant, "USD")
+                , pdPricingMeasure = measure
+                , pdSalePrice = fst getSale
+                , pdSaleDate = snd getSale
+                }
+        , pIdentifiers =
+            GMerch.ProductIdentifiers
+                { piBrand = "Southern Exposure Seed Exchange"
+                , piMPN = fullSku
+                }
+        , pProductDescription =
+            GMerch.ProductDescription
+                { pdCondition = GMerch.New
+                , pdAdult = False
+                , pdIsBundle = any ("Mixes" `T.isInfixOf`) categoryHierarchy
+                , pdItemGroupId = Just $ productBaseSku prod
+                }
+        }
+  where
+    -- TODO: Eventually move some of this into CommonData to reduce query
+    -- count during product price calculations(Issue #1570).
+    getSale :: (Maybe (Scientific, T.Text), Maybe (UTCTime, UTCTime))
+    getSale =
+        let
+            variantSale =
+                listToMaybe
+                    $ filter
+                        ((== variantId) . productSaleProductVariantId)
+                        prodSales
+            categoryIds =
+                concatMap (\cId -> map entityKey $ queryCategoryPredecessorCache cId predCache)
+                    $ productCategoryIds prod
+            categorySale =
+                listToMaybe
+                    $ filter (any (`elem` categoryIds) . categorySaleCategoryIds)
+                        catSales
+            price =
+                productVariantPrice variant
+            makeVariantSale vSale =
+                ( Just
+                    ( toDollars $ productSalePrice vSale
+                    , "USD"
+                    )
+                , Just
+                    ( productSaleStartDate vSale
+                    , productSaleEndDate vSale
+                    )
+                )
+            makeCategorySale cSale =
+                ( Just
+                    ( toDollars $ categorySalePrice price cSale
+                    , "USD"
+                    )
+                , Just
+                    ( categorySaleStartDate cSale
+                    , categorySaleEndDate cSale
+                    )
+                )
+        in
+        case (variantSale, categorySale) of
+            (Just vSale, Just cSale) ->
+                if productSalePrice vSale < categorySalePrice price cSale then
+                    makeVariantSale vSale
+                else
+                    makeCategorySale cSale
+            (Just vSale, Nothing) ->
+                makeVariantSale vSale
+            (_, Just cSale) ->
+                makeCategorySale cSale
+            _ ->
+                (Nothing, Nothing)
+    makeHierarchy :: CategoryId -> [T.Text]
+    makeHierarchy cId =
+        let
+            name = maybe "" categoryName $ M.lookup cId categoryMap
+            parents = queryCategoryPredecessorCache cId predCache
+            parentNames = map (categoryName . entityVal) parents
+        in
+        reverse $ name : parentNames
+
+
+lotSizeToMeasurement :: LotSize -> Maybe GMerch.PricingMeasure
+lotSizeToMeasurement = \case
+        CustomLotSize _ ->
+            Nothing
+        Mass (Milligrams mg) ->
+            Just $ GMerch.Grams (scientific (fromIntegral mg) (-3))
+        Bulbs ct ->
+            Just $ GMerch.Count ct
+        Slips ct ->
+            Just $ GMerch.Count ct
+        Plugs ct ->
+            Just $ GMerch.Count ct
