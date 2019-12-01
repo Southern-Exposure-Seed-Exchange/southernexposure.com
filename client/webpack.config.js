fc9fe9ecda7f2ea5cc89f52b42b4aff5316ca51e
Pavan Rikhi <pavan.rikhi@gmail.com>
2019-12-01 17:08:42 -0500

    Fix Typo in CSP Configuration

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -195,7 +195,7 @@ module.exports = {
       'object-src': "'none'",
       'connect-src': ["'self'", "https://checkout.stripe.com"],
       'frame-src': ["'self'", "https://checkout.stripe.com", "https://www.farmraiser.com"],
-      'script-src': ["'self'", "'unsafe-inline'", "'unsafe-eval'", "https://www.googletagmanager.com", "https://google-analaytics.com/", "https://checkout.stripe.com"],
+      'script-src': ["'self'", "'unsafe-inline'", "'unsafe-eval'", "https://www.googletagmanager.com", "https://google-analytics.com/", "https://checkout.stripe.com"],
       'img-src': ["'self'", "data:", "https://www.googletagmanager.com", "https://*.stripe.com"],
       'font-src': ["'self'", "https://fonts.gstatic.com"],
       'style-src': ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
